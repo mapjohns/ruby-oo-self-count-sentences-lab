2f9d4a003c72254d5434dd971f2e2d3b2e6d7909
Matthew <mattj20@gmail.com>
2022-05-24 07:19:13 -0700

    commit tree dfcdea60eb1eaadbf2db68c140a9abfbcb083d53

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    clean_up =self.squeeze
+    count = clean_up.scan(/[(.|?|!)]/)
+    count.size
   end
 end
